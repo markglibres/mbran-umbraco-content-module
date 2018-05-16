c575166eb8c5a19546d26f9378ecaa4f51042136
Mark Gil Libres <markglibres@gmail.com>
2018-05-16 23:58:17 +1000

    update sample controller / views / model

---
+++ src/MBran.ContentModule.Web.Test/Models/HeroBanner.cs
@@ -0,0 +1,9 @@
+﻿using MBran.ContentModule.Models;
+
+namespace MBran.ContentModule.Web.Test.Models
+{
+    public class HeroBanner : ModuleModel
+    {
+        public string TextMediaTitle { get; set; }
+    }
+}
