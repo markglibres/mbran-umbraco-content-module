80aabaeb4cab1d91f74f146a31f9b2828b556f85
Mark Gil Libres <markglibres@gmail.com>
2018-05-22 13:53:55 +1000

    updated project files

---
+++ src/MBran.ContentModule/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using Umbraco.Core;
+
+namespace MBran.ContentModule.Helpers
+{
+    public class ConfigurationHelper
+    {
+        private const string _defaultRootPath = "~/Views/Partials/Content";
+        private const string _rootPathConfigKey = "MBran.ContentModule.Root";
+
+        private static readonly Lazy<ConfigurationHelper> _instance =
+            new Lazy<ConfigurationHelper>(() => new ConfigurationHelper());
+
+        private ConfigurationHelper()
+        {
+        }
+
+        public static ConfigurationHelper Instance => _instance.Value;
+
+        public string GetRootPath()
+        {
+            var cacheName = string.Join("_", GetType().FullName, nameof(GetRootPath));
+
+            return (string) ApplicationContext.Current.ApplicationCache
+                .RuntimeCache
+                .GetCacheItem(cacheName, () =>
+                {
+                    var configValue = ConfigurationManager.AppSettings[_rootPathConfigKey];
+                    return string.IsNullOrWhiteSpace(configValue) ? _defaultRootPath : configValue.TrimEnd('/');
+                });
+        }
+    }
+}
